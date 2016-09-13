ab2dd51216cc97ec36c9bafaf71186044b487d43
Kirill Berezin <enelar@exsul.net>
2016-09-13 18:04:36 +0300

    Add reactor components support

--- src/zyzo/MeteorDDP/Reactor.php
+++ src/zyzo/MeteorDDP/Reactor.php
@@ -3,6 +3,7 @@
 class reactor extends protocol/reader
 {
   protected $reactor = [];
+  private $component_ids = [];
 
   public function react()
   {
@@ -16,5 +17,22 @@ class reactor extends protocol/reader
   public function on($type, $value)
   {
     return $this->react->$type($value);
+  }
+
+  public function add_component($type, $component)
+  {
+    $id = $this->get_commonent_id($type);
+
+    $this->reactor[$type][$id] = $component;
+
+    return $id;
+  }
+
+  private function get_commonent_id($type)
+  {
+    if (!isset($this->component_ids[$type]))
+      $this->component_ids[$type] = 0;
+
+    return $this->component_ids[$type];
   }
 }
